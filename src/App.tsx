e149392f2fc8293550803b680bdb6d4e6f858d39
Frank William Daniels <frank.william.daniels@gmail.com>
2025-01-19 16:24:57 +0100

    Add markers on map click

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import GoogleMaps from './lib/googlemaps/GoogleMaps';
+import { AdvancedMarker } from '@vis.gl/react-google-maps';
+
+
+
 
 function App() {
+
+  const [markers, setMarkers] = useState([
+    <AdvancedMarker
+      position={{ lat: 63.429, lng: 10.392 }}
+      clickable={true}
+      onClick={() => console.log('Marker clicked')}
+    />,
+    <AdvancedMarker
+      position={{ lat: 63.529, lng: 10.292 }}
+      clickable={true}
+      onClick={() => console.log('Marker clicked')}
+    />
+  ]);
+
+  function OnMapClick(e: any) {
+    console.log('Map clicked', e.detail.latLng);
+    setMarkers((prevMarkers) => [
+      ...prevMarkers,
+      <AdvancedMarker
+        position={{ lat: e.detail.latLng.lat, lng: e.detail.latLng.lng }}
+        clickable={true}
+        onClick={() => console.log('Marker clicked')}
+      />
+    ]);
+  }
+
   return (
     <div className="App">
-      <GoogleMaps />
+      <GoogleMaps markers={markers} onMapClick={OnMapClick}/>
     </div>
   );
 }
--- src/lib/googlemaps/GoogleMaps.tsx
+++ src/lib/googlemaps/GoogleMaps.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import {APIProvider, Map,  AdvancedMarker} from '@vis.gl/react-google-maps';
 
 
 const api_key= process.env.REACT_APP_GOOGLE_API_KEY || '';
+const mapId = '64b9cff6747cc800';
 
- const GoogleMaps = () => (
-  <APIProvider apiKey={api_key}>
+type props = {
+  markers: typeof AdvancedMarker[];
+  onMapClick: (e: any) => void;
+}
+
+ const GoogleMaps = (props: props) => (  
+  <APIProvider apiKey={api_key} >
     <Map
+      colorScheme = 'DARK'
+      mapId = {mapId}
       style={{width: '100vw', height: '100vh'}}
-      defaultCenter={{lat: 22.54992, lng: 0}}
-      defaultZoom={3}
+      defaultCenter={{lat: 63.429, lng: 10.392}}
+      defaultZoom={9}
       gestureHandling={'greedy'}
       disableDefaultUI={true}
-    />
+      onClick={props.onMapClick}
+       >
+     
+      {props.markers}
+
+    </Map>
   </APIProvider>
 );
+
+
 
 export default GoogleMaps;
